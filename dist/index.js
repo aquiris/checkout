0d07bb150575ca8e5e042bb08941940e637ad4db
Thiago Guardado Beltran <thiago.guardado@aquiris.com.br>
2023-02-23 11:27:24 -0300

    Increases default max retries for git operations

--- dist/index.js
+++ dist/index.js
@@ -14004,7 +14004,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.execute = exports.RetryHelper = void 0;
 const core = __importStar(__webpack_require__(470));
-const defaultMaxAttempts = 3;
+const defaultMaxAttempts = 5;
 const defaultMinSeconds = 10;
 const defaultMaxSeconds = 20;
 class RetryHelper {
